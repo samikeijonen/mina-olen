eee5fb4362b04a18924b0e00b1ca527fd89d3add
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-01-26 22:14:48 +0200

    Better admin preview and some filters added.

--- includes/custom-header.php
+++ includes/custom-header.php
@@ -86,10 +86,13 @@ function mina_olen_custom_header_wp_head() {
 	$header_width = get_custom_header()->width;
 	
 	// When to show header image
-	$min_width = apply_filters( 'mina_olen_header_bg_show', 1 );
+	$min_width = absint( apply_filters( 'mina_olen_header_bg_show', 1 ) );
+	
+	// Background arguments
+	$background_arguments = esc_attr( apply_filters( 'mina_olen_header_bg_arguments', 'no-repeat scroll top' ) );
 
 	$style = "body.custom-header #site-title a { color: #{$hex}; }";
-	$style .= "@media screen and (min-width: {$min_width}px) { body.custom-header #header { background: url({$header_image}) no-repeat scroll top; background-size: {$header_width}px auto; min-height: {$header_height}px; } }";
+	$style .= "@media screen and (min-width: {$min_width}px) { body.custom-header #header { background: url({$header_image}) {$background_arguments}; background-size: {$header_width}px auto; min-height: {$header_height}px; } }";
 
 	echo "\n" . '<style type="text/css" id="custom-header-css">' . trim( $style ) . '</style>' . "\n";
 }
@@ -103,32 +106,30 @@ function mina_olen_custom_header_wp_head() {
  */
 function mina_olen_custom_header_admin_preview() { ?>
 
-		<div <?php hybrid_attr( 'body' ); // Fake <body> class. ?>>
+	<div <?php hybrid_attr( 'body' ); // Fake <body> class. ?>>
 
-			<header <?php hybrid_attr( 'header' ); ?>>
+		<header <?php hybrid_attr( 'header' ); ?>>
+		
+			<div class="wrap">
+			
+			<?php if ( display_header_text() ) { // If user chooses to display header text. ?>
 
-				<?php if ( display_header_text() ) : // If user chooses to display header text. ?>
+				<div id="branding">
+					<h1 id="site-title">
+						<a href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" rel="home">
+							<?php bloginfo( 'name' ); ?>
+						</a>
+					</h1>
+					<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+				</div><!-- #branding -->
+				
+			<?php } ?>
+				
+			</div><!-- .wrap -->
 
-					<div id="branding">
-						<?php hybrid_site_title(); ?>
-						<?php hybrid_site_description(); ?>
-					</div><!-- #branding -->
+		</header><!-- #header -->
 
-				<?php endif; // End check for header text. ?>
-
-			</header><!-- #header -->
-
-			<?php if ( get_header_image() && !display_header_text() ) : // If there's a header image but no header text. ?>
-
-				<a href="<?php echo home_url(); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" rel="home"><img class="header-image" src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a>
-
-			<?php elseif ( get_header_image() ) : // If there's a header image. ?>
-
-				<img class="header-image" src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" />
-
-			<?php endif; // End check for header image. ?>
-
-		</div><!-- Fake </body> close. -->
+	</div><!-- Fake </body> close. -->
 
 <?php }
 
@@ -146,8 +147,27 @@ function mina_olen_custom_header_admin_head() {
 
 	if ( empty( $hex ) )
 		return;
+		
+	// Header image
+	$header_image = esc_url( get_header_image() );
+	
+	// Header image height
+	$header_height = get_custom_header()->height;
+	
+	// Header image width
+	$header_width = get_custom_header()->width;
+	
+	// When to show header image
+	$min_width = apply_filters( 'mina_olen_header_bg_show', 1 );
+	
+	// Background arguments
+	$background_arguments = apply_filters( 'mina_olen_header_bg_arguments', 'no-repeat scroll top' );
 
 	$style = "#site-title a { color: #{$hex}; }";
-
+	
+	if ( get_header_image() ) {
+		$style .= ".custom-header #header { background: url({$header_image}) {$background_arguments}; background-size: {$header_width}px auto; min-height: {$header_height}px; }";
+	}
+	
 	echo "\n" . '<style type="text/css" id="custom-header-css">' . trim( $style ) . '</style>' . "\n";
 }
