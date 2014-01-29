7c8daa479ebb45d094b654b46bab84326807dddc
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-01-29 22:19:00 +0200

    Remove files.

--- sidebar-secondary.php
+++
@@ -1,9 +0,0 @@
-<?php if ( is_active_sidebar( 'secondary' ) ) { ?>
-
-	<aside id="sidebar-secondary" <?php hybrid_attr( 'sidebar', 'secondary' ); ?>>
-
-		<?php dynamic_sidebar( 'secondary' ); ?>
-
-	</aside><!-- #sidebar-secondary .aside -->
-
-<?php } ?>
