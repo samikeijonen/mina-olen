4369cb9aac37f1ce837714f47a8c9b950975c144
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-02-08 17:53:06 +0200

    Add in 404 page text and search form.

--- loop-error.php
+++ loop-error.php
@@ -5,7 +5,20 @@
 	</header>
 
 	<div <?php hybrid_attr( 'entry-content' ); ?>>
-		<p><?php _e( 'Apologies, but no entries were found.', 'mina-olen' ); ?></p>
+	
+		<?php if ( is_404() ): // Check if we are on 404 page. ?>
+		
+			<p>
+				<?php printf( __( "You tried going to %s, which doesn't exist. You can try navigate or search.", 'mina-olen' ), '<code>' . home_url( esc_url( $_SERVER['REQUEST_URI'] ) ) . '</code>' ); ?>
+			</p>
+		<?php get_search_form(); // Loads the searchform.php template.
+			
+		else: ?>
+	
+			<p><?php _e( 'Apologies, but no entries were found.', 'mina-olen' ); ?></p>
+	
+		<?php endif; // End check if we are on 404 page. ?>
+	
 	</div><!-- .entry-content -->
 
 </article><!-- .entry -->
