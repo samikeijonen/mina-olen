d1fb15cd180197a32971400c2a799b9163eaf3a7
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-06-20 16:08:15 +0300

    Update theme updater in latest version what we have done with Devin.

---
+++ theme-updater/theme-updater-admin.php
@@ -0,0 +1,403 @@
+<?php
+/**
+ * Theme updater admin page and functions.
+ *
+ * @package EDD Theme Updater
+ */
+
+class EDD_Theme_Updater_Admin {
+
+	/**
+	 * Variables required for the theme updater
+	 *
+	 * @since 1.0.0
+	 * @type string
+	 */
+	 protected $remote_api_url = null;
+	 protected $theme_slug = null;
+	 protected $version = null;
+	 protected $author = null;
+
+	/**
+	 * Initialize the class.
+	 *
+	 * @since 1.0.0
+	 */
+	function __construct( $args = array() ) {
+
+		$args = wp_parse_args( $args, array(
+			'remote_api_url' => 'http://easydigitaldownloads.com',
+			'theme_slug' => get_template(),
+			'item_name' => '',
+			'license' => '',
+			'version' => '',
+			'author' => ''
+		) );
+		extract( $args );
+
+		// Set args
+		$this->remote_api_url = $remote_api_url;
+		$this->item_name = $item_name;
+		$this->theme_slug = sanitize_key( $theme_slug );
+		$this->version = $version;
+		$this->author = $author;
+
+		// Populate fallbacks
+		if ( '' == $version ) {
+			$theme = wp_get_theme( $this->theme_slug );
+			$this->version = $theme->get( 'Version' );
+		}
+
+		add_action( 'admin_init', array( $this, 'updater' ) );
+		add_action( 'admin_init', array( $this, 'register_option' ) );
+		add_action( 'admin_init', array( $this, 'license_action' ) );
+		add_action( 'admin_menu', array( $this, 'license_menu' ) );
+		add_action( 'update_option_' . $this->theme_slug . '_license_key', array( $this, 'activate_license' ), 10, 2 );
+		add_filter( 'http_request_args', array( $this, 'disable_wporg_request' ), 5, 2 );
+
+	}
+
+	/**
+	 * Creates the updater class.
+	 *
+	 * since 1.0.0
+	 */
+	function updater() {
+
+		/* If there is no valid license key status, don't allow updates. */
+		if ( get_option( $this->theme_slug . '_license_key_status', false) != 'valid' ) {
+			return;
+		}
+
+		if ( !class_exists( 'EDD_Theme_Updater' ) ) {
+			// Load our custom theme updater
+			include( dirname( __FILE__ ) . '/theme-updater-class.php' );
+		}
+
+		new EDD_Theme_Updater( array(
+				'remote_api_url' => $this->remote_api_url,
+				'version'        => $this->version,
+				'license'        => trim( get_option( $this->theme_slug . '_license_key' ) ),
+				'item_name'      => $this->item_name,
+				'author'         => $this->author
+			)
+		);
+	}
+
+	/**
+	 * Adds a menu item for the theme license under the appearance menu.
+	 *
+	 * since 1.0.0
+	 */
+	function license_menu() {
+		add_theme_page(
+			__( 'Theme License', 'mina-olen' ),
+			__( 'Theme License', 'mina-olen' ),
+			'manage_options',
+			$this->theme_slug . '-license',
+			array( $this, 'license_page' )
+		);
+	}
+
+	/**
+	 * Outputs the markup used on the theme license page.
+	 *
+	 * since 1.0.0
+	 */
+	function license_page() {
+
+		$license = trim( get_option( $this->theme_slug . '_license_key' ) );
+		$status = get_option( $this->theme_slug . '_license_key_status', false );
+
+		// Checks license status to display under license key
+		if ( ! $license ) {
+			$message    = __( 'Enter your theme license key.', 'mina-olen' );
+		} else {
+			// delete_transient( $this->theme_slug . '_license_message' );
+			if ( ! get_transient( $this->theme_slug . '_license_message', false ) ) {
+				set_transient( $this->theme_slug . '_license_message', $this->check_license(), ( 60 * 60 * 24 ) );
+			}
+			$message = get_transient( $this->theme_slug . '_license_message' );
+		}
+		?>
+		<div class="wrap">
+			<h2><?php _e( 'Theme License', 'mina-olen' ); ?></h2>
+			<form method="post" action="options.php">
+
+				<?php settings_fields( $this->theme_slug . '-license' ); ?>
+
+				<table class="form-table">
+					<tbody>
+
+						<tr valign="top">
+							<th scope="row" valign="top">
+								<?php _e( 'License Key', 'mina-olen' ); ?>
+							</th>
+							<td>
+								<input id="<?php echo $this->theme_slug; ?>_license_key" name="<?php echo $this->theme_slug; ?>_license_key" type="text" class="regular-text" value="<?php echo esc_attr( $license ); ?>" />
+								<p class="description">
+									<?php echo $message; ?>
+								</p>
+							</td>
+						</tr>
+
+						<?php if ( $license ) { ?>
+						<tr valign="top">
+							<th scope="row" valign="top">
+								<?php _e( 'License Action', 'mina-olen' ); ?>
+							</th>
+							<td>
+								<?php
+								wp_nonce_field( $this->theme_slug . '_nonce', $this->theme_slug . '_nonce' );
+								if ( 'valid' == $status ) { ?>
+									<input type="submit" class="button-secondary" name="<?php echo $this->theme_slug; ?>_license_deactivate" value="<?php esc_attr_e( 'Deactivate License', 'mina-olen' ); ?>"/>
+								<?php } else { ?>
+									<input type="submit" class="button-secondary" name="<?php echo $this->theme_slug; ?>_license_activate" value="<?php esc_attr_e( 'Activate License', 'mina-olen' ); ?>"/>
+								<?php }
+								?>
+							</td>
+						</tr>
+						<?php } ?>
+
+					</tbody>
+				</table>
+				<?php submit_button(); ?>
+			</form>
+		<?php
+	}
+
+	/**
+	 * Registers the option used to store the license key in the options table.
+	 *
+	 * since 1.0.0
+	 */
+	function register_option() {
+		register_setting(
+			$this->theme_slug . '-license',
+			$this->theme_slug . '_license_key',
+			array( $this, 'sanitize_license' )
+		);
+	}
+
+	/**
+	 * Sanitizes the license key.
+	 *
+	 * since 1.0.0
+	 *
+	 * @param string $new License key that was submitted.
+	 * @return string $new Sanitized license key.
+	 */
+	function sanitize_license( $new ) {
+
+		$old = get_option( $this->theme_slug . '_license_key' );
+
+		if ( $old && $old != $new ) {
+			// New license has been entered, so must reactivate
+			delete_option( $this->theme_slug . '_license_key_status' );
+			delete_transient( $this->theme_slug . '_license_message' );
+		}
+
+		return $new;
+	}
+
+	/**
+	 * Makes a call to the API.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $api_params to be used for wp_remote_get.
+	 * @return array $response decoded JSON response.
+	 */
+	 function get_api_response( $api_params ) {
+
+		 // Call the custom API.
+		$response = wp_remote_get(
+			add_query_arg( $api_params, $this->remote_api_url ),
+			array( 'timeout' => 15, 'sslverify' => false )
+		);
+
+		// Make sure the response came back okay.
+		if ( is_wp_error( $response ) ) {
+			return false;
+		}
+
+		$response = json_decode( wp_remote_retrieve_body( $response ) );
+
+		return $response;
+	 }
+
+	/**
+	 * Activates the license key.
+	 *
+	 * @since 1.0.0
+	 */
+	function activate_license() {
+
+		$license = trim( get_option( $this->theme_slug . '_license_key' ) );
+
+		// Data to send in our API request.
+		$api_params = array(
+			'edd_action' => 'activate_license',
+			'license'    => $license,
+			'item_name'  => urlencode( $this->item_name )
+		);
+
+		$license_data = $this->get_api_response( $api_params );
+
+		// $response->license will be either "active" or "inactive"
+		if ( $license_data && isset( $license_data->license ) ) {
+			update_option( $this->theme_slug . '_license_key_status', $license_data->license );
+			delete_transient( $this->theme_slug . '_license_message' );
+		}
+
+	}
+
+	/**
+	 * Deactivates the license key.
+	 *
+	 * @since 1.0.0
+	 */
+	function deactivate_license() {
+
+		// Retrieve the license from the database.
+		$license = trim( get_option( $this->theme_slug . '_license_key' ) );
+
+		// Data to send in our API request.
+		$api_params = array(
+			'edd_action' => 'deactivate_license',
+			'license'    => $license,
+			'item_name'  => urlencode( $this->item_name )
+		);
+
+		$license_data = $this->get_api_response( $api_params );
+
+		// $license_data->license will be either "deactivated" or "failed"
+		if ( $license_data && ( $license_data->license == 'deactivated' ) ) {
+			delete_option( $this->theme_slug . '_license_key_status' );
+			delete_transient( $this->theme_slug . '_license_message' );
+		}
+	}
+
+	/**
+	 * Checks if a license action was submitted.
+	 *
+	 * @since 1.0.0
+	 */
+	function license_action() {
+
+		if ( isset( $_POST[ $this->theme_slug . '_license_activate' ] ) ) {
+			if ( check_admin_referer( $this->theme_slug . '_nonce', $this->theme_slug . '_nonce' ) ) {
+				$this->activate_license();
+			}
+		}
+
+		if ( isset( $_POST[$this->theme_slug . '_license_deactivate'] ) ) {
+			if ( check_admin_referer( $this->theme_slug . '_nonce', $this->theme_slug . '_nonce' ) ) {
+				$this->deactivate_license();
+			}
+		}
+
+	}
+
+	/**
+	 * Checks if license is valid and gets expire date.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string $message License status message.
+	 */
+	function check_license() {
+
+		$license = trim( get_option( $this->theme_slug . '_license_key' ) );
+
+		$api_params = array(
+			'edd_action' => 'check_license',
+			'license'    => $license,
+			'item_name'  => urlencode( $this->item_name )
+		);
+
+		$license_data = $this->get_api_response( $api_params );
+
+		// If response doesn't include license data, return
+		if ( !isset( $license_data->license ) ) {
+			$message = __( 'License status is unknown.', 'mina-olen' );
+			return $message;
+		}
+
+		// Get expire date
+		$expires = false;
+		if ( isset( $license_data->expires ) ) {
+			$expires = date_i18n( get_option( 'date_format' ), strtotime( $license_data->expires ) );
+			$renew_link = '<a href="' . esc_url( $this->remote_api_url) . '">' . __( 'Renew?', 'mina-olen' ) . '</a>';
+		}
+
+		// Get site counts
+		$site_count = $license_data->site_count;
+		$license_limit = $license_data->license_limit;
+
+		// If unlimited
+		if ( 0 == $license_limit ) {
+			$license_limit = __( 'unlimited', 'mina-olen' );
+		}
+
+		if ( $license_data->license == 'valid' ) {
+			$message = __( 'License key is active.', 'mina-olen' ) . ' ';
+			if ( $expires ) {
+				$message .= sprintf( __( 'Expires %s.', 'mina-olen' ), $expires ) . ' ';
+			}
+			if ( $site_count && $license_limit ) {
+				$message .= sprintf( __( 'You have %1$s / %2$s sites activated.', 'mina-olen' ), $site_count, $license_limit );
+			}
+		} else if ( $license_data->license == 'expired' ) {
+			if ( $expires ) {
+				$message = sprintf( __( 'License key expired %s.', 'mina-olen' ), $expires );
+			} else {
+				$message = __( 'License key has expired.', 'mina-olen' );
+			}
+			if ( $renew_link ) {
+				$message .= ' ' . $renew_link;
+			}
+		} else if ( $license_data->license == 'invalid' ) {
+			$message = __( 'License keys do not match.', 'mina-olen' );
+		} else if ( $license_data->license == 'inactive' ) {
+			$message = __( 'License is inactive.', 'mina-olen' );
+		} else if ( $license_data->license == 'disabled' ) {
+			$message = __( 'License key is disabled.', 'mina-olen' );
+		} else if ( $license_data->license == 'site_inactive' ) {
+			// Site is inactive
+			$message = __( 'Site is inactive.', 'mina-olen' );
+		} else {
+			$message = __( 'License status is unknown.', 'mina-olen' );
+		}
+
+		return $message;
+	}
+
+	/**
+	 * Disable requests to wp.org repository for this theme.
+	 *
+	 * @since 1.0.0
+	 */
+	function disable_wporg_request( $r, $url ) {
+
+		// If it's not a theme update request, bail.
+		if ( 0 !== strpos( $url, 'https://api.wordpress.org/themes/update-check/1.1/' ) ) {
+ 			return $r;
+ 		}
+
+ 		// Decode the JSON response
+ 		$themes = json_decode( $r['body']['themes'] );
+
+ 		// Remove the active parent and child themes from the check
+ 		$parent = get_option( 'template' );
+ 		$child = get_option( 'stylesheet' );
+ 		unset( $themes->themes->$parent );
+ 		unset( $themes->themes->$child );
+
+ 		// Encode the updated JSON response
+ 		$r['body']['themes'] = json_encode( $themes );
+
+ 		return $r;
+	}
+
+}
---
+++ theme-updater/theme-updater.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * One-click updater for the Mina Olen Theme
+ *
+ * @package EDD Theme Updater
+ */
+
+// Includes the files needed for the theme updater
+if ( !class_exists( 'EDD_Theme_Updater_Admin' ) ) {
+	include( dirname( __FILE__ ) . '/theme-updater-admin.php' );
+}
+
+// Loads the updater classes
+$updater = new EDD_Theme_Updater_Admin(
+
+	array(
+		'remote_api_url' => 'http://foxnet-themes.fi', // Site where EDD is hosted
+		'item_name'      => 'Mina olen',               // Name of theme
+		'theme_slug'     => 'mina_olen_theme',         // Theme slug
+		'version'        => MINA_OLEN_VERSION,         // The current version of this theme
+		'author'         => 'Sami Keijonen'            // The author of this theme
+	)
+
+);
