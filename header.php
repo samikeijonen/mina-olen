924ac288a8af39e3bf3f36f4fbf62dd0147224e8
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-12-26 20:09:01 +0200

    Add filter when to show header.

--- header.php
+++ header.php
@@ -20,39 +20,52 @@
 	
 		<?php get_sidebar( 'header' ); // Loads the sidebar-header.php template. ?>
 	
-		<?php get_template_part( 'menu', 'primary' ); // Loads the menu-primary.php template. ?>
+		<?php get_template_part( 'menu', 'primary' ); // Loads the menu-primary.php template.
 		
-		<header <?php hybrid_attr( 'header' ); ?>>
+		/* When to show header at all. */
+		$mina_olen_show_masthead = apply_filters( 'mina_olen_show_masthead', true );
 		
-			<div class="wrap">
-			
-			<?php if ( display_header_text() ) { // If user chooses to display header text. ?>
+		if( $mina_olen_show_masthead ) : // Check do we show header. ?>
+		
+			<header <?php hybrid_attr( 'header' ); ?>>
 
-				<div id="branding">
-					<?php if ( get_theme_mod( 'logo_upload') ) { // Use logo if is set. Else use bloginfo name. ?>	
-						<h1 id="site-title">
-							<a href="<?php echo esc_url( home_url() ); ?>" rel="home">
-								<img class="mina-olen-logo" src="<?php echo esc_url( get_theme_mod( 'logo_upload' ) ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" />
-							</a>
-						</h1>
-					<?php } else { ?>
-						<h1 id="site-title">
-							<a href="<?php echo esc_url( home_url() ); ?>" rel="home">
-								<?php bloginfo( 'name' ); ?>
-							</a>
-						</h1>
-					<?php } ?>
-					<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
-				</div><!-- #branding -->
-				
-			<?php }
+				<div class="wrap">
 			
-				/* Callout text and link in singular pages. */
-				mina_olen_callout_output(); ?>
-				
-			</div><!-- .wrap -->
+				<?php if ( display_header_text() ) { // If user chooses to display header text. ?>
 
-		</header><!-- #header -->
+					<div id="branding">
+						<?php if ( get_theme_mod( 'logo_upload') ) { // Use logo if is set. Else use bloginfo name. ?>	
+							<h1 id="site-title">
+								<a href="<?php echo esc_url( home_url() ); ?>" rel="home">
+									<img class="mina-olen-logo" src="<?php echo esc_url( get_theme_mod( 'logo_upload' ) ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" />
+								</a>
+							</h1>
+						<?php } else { ?>
+							<h1 id="site-title">
+								<a href="<?php echo esc_url( home_url() ); ?>" rel="home">
+									<?php bloginfo( 'name' ); ?>
+								</a>
+							</h1>
+						<?php } ?>
+						<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+					</div><!-- #branding -->
+				
+				<?php }
+			
+					/* Callout text and link in singular pages. */
+					mina_olen_callout_output();
+					
+					do_action( 'mina_olen_header_close' );
+					
+				?>
+				
+				</div><!-- .wrap -->
+
+			</header><!-- #header -->
+			
+		<?php endif; // End check for header. ?>
+		
+		<?php do_action( 'mina_olen_after_header' ); ?>
 
 		<?php if ( current_theme_supports( 'breadcrumb-trail' ) ) {
 			 
